544bbcb86c270f82f973d76ae23698bee3e58772
HoaiNampro <namn2591@gmail.com>
2023-11-02 22:03:55 +0700

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.Home.as_view(),name='home'),
+    path('login/',views.Login.as_view(),name='login'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
-def home(request):
-    return render(request,'myapp/home.html')
+class Home(View):
+    def get(self,request):
+        return render(request,'myapp/home.html')
+class Login(View):
+    def get(self,request):
+        return render(request,'myapp/login.html')
+    def post(self,request):
+        user_name = request.POST.get("user_name")
+        password = request.POST.get("password")
+        user = authenticate(username=user_name,password=password)
+        if user is None:
+            return  render(request,'myapp/login.html')
+        login(request,user)
+        return render(request,'myapp/susscess_login.html')
+
