d0ae3468cf05e8065ae4b207f2a5faa3adde3ad1
HoaiNampro <namn2591@gmail.com>
2023-11-05 12:13:14 +0700

    update

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    feature_vector = models.JSONField()
+    ip_address = models.GenericIPAddressField()
+    user_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
-
+from retinaface import RetinaFace
+import numpy as np
+import cv2
 # Create your views here.
 class Home(View):
     def get(self,request):
@@ -21,10 +23,24 @@ class Login(View):
         login(request,user)
         return render(request,'myapp/susscess_login.html')
 
+def detect_faces(frame):
+    obj = RetinaFace.detect_faces(frame)
+    for key in obj.keys():
+        identify = obj[key]
+        facial_area = identify['facial_area']
+        cv2.rectangle(frame, (facial_area[2], facial_area[3]), (facial_area[0], facial_area[1]), (0, 255, 0), 2)
+    return frame
+
 class  ProcessImage(View):
     def post(self,request):
         if request.method == 'POST':
                 image_data = request.FILES['image'].read()
-                encoded_image = base64.b64encode(image_data).decode('utf-8')
-                return render(request, 'myapp/processed_image.html', {'image': encoded_image})
+                nparr = np.frombuffer(image_data, np.uint8)
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                processed_img =detect_faces(img)
+                _, img_encoded = cv2.imencode('.jpg', processed_img)
+                img_base64 = base64.b64encode(img_encoded).decode('utf-8')
+                return render(request, 'myapp/processed_image.html', {'image':img_base64})
         return render(request, 'myapp/susscess_login.html')
+    def get(self,request):
+        pass
